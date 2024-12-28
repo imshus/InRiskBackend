1ce7afa1284124e4b8ebcadf4eea9f1ba1f31ad1
imshus <67273421+imshus@users.noreply.github.com>
2024-12-28 16:50:02 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,7 +1,6 @@
-from app import app
+from app import app 
 
 def test_home():
-    response=app.text_client().get("/")
-
-    assert response.status_code==200
-    assert response.data==b"Hello World"
+    with app.test_client() as client: 
+        response = client.get("/")
+        assert response.status_code == 200
