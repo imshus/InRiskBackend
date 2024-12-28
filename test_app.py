fca5686c1c95756a9b79eb515d101946bc1141f8
imshus <67273421+imshus@users.noreply.github.com>
2024-12-28 16:58:01 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -2,5 +2,9 @@ from app import app
 
 def test_home():
     with app.test_client() as client: 
-        response = client.get("/")
+        
+        response=client.post("/store-weather-data")
+        response=client.get("/list-weather-files")
+        response=client.get("/weather-file-content/<file_name>")
+        
         assert response.status_code == 200
